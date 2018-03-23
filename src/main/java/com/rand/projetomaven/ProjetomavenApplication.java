7c36d9f8eb6e9fb6b92779658da3587c6d2181ad
Randsson <randssondouglas@gmail.com>
2018-03-23 15:27:51 -0300

    initializing the project

---
+++ src/main/java/com/rand/projetomaven/ProjetomavenApplication.java
@@ -0,0 +1,12 @@
+package com.rand.projetomaven;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetomavenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetomavenApplication.class, args);
+	}
+}
---
+++ src/test/java/com/rand/projetomaven/ProjetomavenApplicationTests.java
@@ -0,0 +1,16 @@
+package com.rand.projetomaven;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ProjetomavenApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
