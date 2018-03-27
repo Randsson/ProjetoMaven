625117a1b7684319b7514e378a03e5af7f31b3bf
Randsson <randssondouglas@gmail.com>
2018-03-27 14:52:49 -0300

    create Product class and associations

--- src/main/java/com/rand/projetomaven/ProjetomavenApplication.java
+++ src/main/java/com/rand/projetomaven/ProjetomavenApplication.java
@@ -1,20 +1,24 @@
 package com.rand.projetomaven;
 
-//import org.assertj.core.util.Arrays;
 import java.util.Arrays;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.rand.projetomaven.domain.Category;
+import com.rand.projetomaven.domain.Product;
 import com.rand.projetomaven.repositories.CategoryRepository;
+import com.rand.projetomaven.repositories.ProductRepository;
 
 @SpringBootApplication
 public class ProjetomavenApplication implements CommandLineRunner {
 	
 	@Autowired
 	private CategoryRepository catRepository;
+	@Autowired
+	private ProductRepository prodRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(ProjetomavenApplication.class, args);
@@ -27,6 +31,18 @@ public class ProjetomavenApplication implements CommandLineRunner {
 		Category cat1 = new Category(null, "informática");
 		Category cat2 = new Category(null, "Escritório");
 		
+		Product p1 = new Product(null, "Computador", 2000.00);
+		Product p2 = new Product(null, "Impressora", 800.00);
+		Product p3 = new Product(null, "Mouse", 80.00);
+		
+		cat1.getProducts().addAll(Arrays.asList(p1, p2, p3));
+		cat2.getProducts().addAll(Arrays.asList(p2));
+		
+		p1.getCategories().addAll(Arrays.asList(cat1));
+		p2.getCategories().addAll(Arrays.asList(cat1, cat2));
+		p3.getCategories().addAll(Arrays.asList(cat1));
+		
 		catRepository.saveAll(Arrays.asList(cat1, cat2));
+		prodRepository.saveAll(Arrays.asList(p1, p2, p3));
 	}
 }
