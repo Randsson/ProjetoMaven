56b34535771eec70a6db49ed3d2a17658ec26af0
Randsson <randssondouglas@gmail.com>
2018-03-27 14:15:23 -0300

    add seed operation for categories

--- src/main/java/com/rand/projetomaven/ProjetomavenApplication.java
+++ src/main/java/com/rand/projetomaven/ProjetomavenApplication.java
@@ -1,12 +1,32 @@
 package com.rand.projetomaven;
 
+//import org.assertj.core.util.Arrays;
+import java.util.Arrays;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.rand.projetomaven.domain.Category;
+import com.rand.projetomaven.repositories.CategoryRepository;
+
 @SpringBootApplication
-public class ProjetomavenApplication {
+public class ProjetomavenApplication implements CommandLineRunner {
+	
+	@Autowired
+	private CategoryRepository catRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(ProjetomavenApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		// TODO Auto-generated method stub
+		
+		Category cat1 = new Category(null, "informática");
+		Category cat2 = new Category(null, "Escritório");
+		
+		catRepository.saveAll(Arrays.asList(cat1, cat2));
 	}
 }
