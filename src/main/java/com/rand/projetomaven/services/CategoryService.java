72fd39145d2d08947a654dc686439d2f4215c813
Randsson <randssondouglas@gmail.com>
2018-03-30 21:20:13 -0300

    add delete resource for Category

--- src/main/java/com/rand/projetomaven/services/CategoryService.java
+++ src/main/java/com/rand/projetomaven/services/CategoryService.java
@@ -3,10 +3,12 @@ package com.rand.projetomaven.services;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
 import com.rand.projetomaven.domain.Category;
 import com.rand.projetomaven.repositories.CategoryRepository;
+import com.rand.projetomaven.services.exceptions.DataIntegrityException;
 import com.rand.projetomaven.services.exceptions.ObjNotFoundException;
 
 @Service
@@ -29,6 +31,16 @@ public class CategoryService {
 	public Category update (Category obj) {
 		find(obj.getId());
 		return repo.save(obj);
+	}
+
+	public void delete(Integer id) {
+		find(id);
+		try {
+		repo.deleteById(id);
+		}
+		catch (DataIntegrityViolationException e) {
+			throw new DataIntegrityException("Não é possível excluir uma categoria que possui produtos");
+		}
 	}
 
 }
