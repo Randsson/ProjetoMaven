2c51916c2db58eabd463224321aa9b1a317e7115
Randsson <randssondouglas@gmail.com>
2018-03-31 18:29:17 -0300

    add 'list all' function for Category using DTO

--- src/main/java/com/rand/projetomaven/resources/CategoryResource.java
+++ src/main/java/com/rand/projetomaven/resources/CategoryResource.java
@@ -1,6 +1,8 @@
 package com.rand.projetomaven.resources;
 
 import java.net.URI;
+import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -12,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.rand.projetomaven.domain.Category;
+import com.rand.projetomaven.dto.CategoryDTO;
 import com.rand.projetomaven.services.CategoryService;
 
 @RestController
@@ -47,5 +50,12 @@ public class CategoryResource {
 	public ResponseEntity<Void> delete(@PathVariable Integer id){
 		service.delete(id);
 		return ResponseEntity.noContent().build();
+	}
+	
+	@RequestMapping(method=RequestMethod.GET)
+	public ResponseEntity <List<CategoryDTO>> findAll() {
+		List<Category> list = service.findAll();
+		List<CategoryDTO> listDTO = list.stream().map(obj -> new CategoryDTO(obj)).collect(Collectors.toList());
+		return ResponseEntity.ok().body(listDTO);
 	}
 }
--- src/main/java/com/rand/projetomaven/services/CategoryService.java
+++ src/main/java/com/rand/projetomaven/services/CategoryService.java
@@ -1,5 +1,6 @@
 package com.rand.projetomaven.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -43,5 +44,8 @@ public class CategoryService {
 		}
 	}
 
+	public List<Category> findAll() {
+		return repo.findAll();
+	}
 }
 	
