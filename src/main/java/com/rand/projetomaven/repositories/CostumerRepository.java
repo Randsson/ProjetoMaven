86a431601dd213382393c75488655267b3acc4c9
Randsson <randssondouglas@gmail.com>
2018-04-09 14:37:41 -0300

    add not repeatable email validation for Costumer

--- src/main/java/com/rand/projetomaven/repositories/CostumerRepository.java
+++ src/main/java/com/rand/projetomaven/repositories/CostumerRepository.java
@@ -1,7 +1,10 @@
 package com.rand.projetomaven.repositories;
 
+
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.rand.projetomaven.domain.Costumer;
 
@@ -9,5 +12,7 @@ import com.rand.projetomaven.domain.Costumer;
 public interface CostumerRepository extends JpaRepository<Costumer, Integer> {
 	
 	
-
+	@Transactional(readOnly = true)
+	Costumer findByEmail(String email);
+	
 }
--- src/main/java/com/rand/projetomaven/services/validation/CostumerInsertValidator.java
+++ src/main/java/com/rand/projetomaven/services/validation/CostumerInsertValidator.java
@@ -6,13 +6,21 @@ import java.util.List;
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.rand.projetomaven.domain.Costumer;
 import com.rand.projetomaven.domain.enums.CostumerKind;
 import com.rand.projetomaven.dto.CostumerNewDTO;
+import com.rand.projetomaven.repositories.CostumerRepository;
 import com.rand.projetomaven.resources.exceptions.FieldMessage;
 import com.rand.projetomaven.services.validation.utils.BR;
 
 public class CostumerInsertValidator implements ConstraintValidator<CostumerInsert, CostumerNewDTO>{
 	
+	
+	@Autowired 
+	private CostumerRepository repo;
+		
 	@Override
 	public void initialize(CostumerInsert ann) {
 		
@@ -30,6 +38,13 @@ public class CostumerInsertValidator implements ConstraintValidator<CostumerInse
 		if(objDTO.getKind().equals(CostumerKind.PESSOAJURIDICA.getCod()) && !BR.isValidCNPJ(objDTO.getCpfOuCnpj())) {
 			list.add(new FieldMessage("cpfOuCnpj", "CNPJ inválido"));
 		}
+		
+		Costumer aux = repo.findByEmail(objDTO.getEmail());
+		
+		if (aux != null) {
+			list.add(new FieldMessage("email", "Email já existe"));
+		}
+		
 		for (FieldMessage e: list) {
 			context.disableDefaultConstraintViolation();
 			context.buildConstraintViolationWithTemplate(e.getMessage()).addPropertyNode(e.getFieldName()).addConstraintViolation();
