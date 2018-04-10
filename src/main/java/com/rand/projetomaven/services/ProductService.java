d8192022682e7ea860d1f12a382b976ae9baf478
Randsson <randssondouglas@gmail.com>
2018-04-10 16:37:45 -0300

    add search by name and category for Product

--- src/main/java/com/rand/projetomaven/services/ProductService.java
+++ src/main/java/com/rand/projetomaven/services/ProductService.java
@@ -1,11 +1,17 @@
 package com.rand.projetomaven.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
+import com.rand.projetomaven.domain.Category;
 import com.rand.projetomaven.domain.Product;
+import com.rand.projetomaven.repositories.CategoryRepository;
 import com.rand.projetomaven.repositories.ProductRepository;
 import com.rand.projetomaven.services.exceptions.ObjNotFoundException;
 
@@ -15,11 +21,20 @@ public class ProductService {
 	@Autowired
 	private ProductRepository repo;
 	
+	@Autowired
+	private CategoryRepository categoryRepository;
+	
 	public Product find(Integer id) {
 		Optional<Product> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjNotFoundException(
 		  "Object not found! Id:" + id + ", Type: "+ Product.class.getName()));			
 	}
 
+	public Page<Product> search(String name, List<Integer> ids, Integer page, Integer linesPerPage, String orderBy, String direction) {
+		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
+		List<Category> categories = categoryRepository.findAllById(ids);
+		return repo.findDistinctByNameContainingAndCategoriesIn(name, categories, pageRequest);
+		
+	}
 }
 	
